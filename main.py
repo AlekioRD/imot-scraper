662c64e4299792dadc4fac06d74799e990e16c1b
AlekioRD <office@aleksandardyakov.com>
2025-06-23 14:19:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Imot.bg Scraper - Placeholder file
+Full code will be added in GitHub
+"""
+
+print("Imot scraper starting...")
+
+# This is a placeholder file
+# Full code will be added after uploading to GitHub
